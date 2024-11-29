766d73454d09719d7182b7b72e0fb87fa70dec32
KarraRohithKumar <karrarohithkumar@gmail.com>
2024-11-29 22:46:25 +0530

    Add files via upload

---
+++ Binary_search.java
@@ -0,0 +1,18 @@
+public class Binary_search {
+    public static void main(String[] args) {
+        
+    
+    int[] nums={1,2,3,4};
+    int target=3;
+    Solution obj=new Solution();
+    System.out.println(obj.search(nums,target));
+}}
+class Solution {
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==target){
+                return i;
+            }
+        }return -1;
+    }
+}
